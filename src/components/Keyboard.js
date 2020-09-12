fdb7e62e401c2ddb65ff24f9857f6ce6225901f2
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-09-12 22:33:04 +0200

    Create a keyboard

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 export default function Keyboard() {
+
+    const firstRowLetters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'];
+    const secondRowLetters = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
+    const thirdRowLetters = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+
+    const firstRowButtons = getButtons(firstRowLetters);
+    const secondRowButtons = getButtons(secondRowLetters);
+    const thirdRowButtons = getButtons(thirdRowLetters);
+
     return (
-        <div>A QWERTY keyboard.</div>
+        <div>
+            <div>{firstRowButtons}</div>
+            <div>{secondRowButtons}</div>
+            <div>{thirdRowButtons}</div>
+        </div>
     );
+}
+
+function getButtons(letters) {
+    return letters.map(letter => {
+        return <button>{letter}</button>
+    })
 }
