3253fd983079b21b8638a6d3fb0564710b1cc2bb
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-09-12 22:22:07 +0200

    Add a picture of spaceman

--- src/components/SpacemanPicture.js
+++ src/components/SpacemanPicture.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function SpacemanPicture() {
     return (
-        <div>The picture of spaceman.</div>
+        <img src="images/spaceman.png" alt="Spaceman"/>
     );
 }
