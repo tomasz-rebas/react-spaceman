281e038f527d767ee1dbb5346d73e466b93585be
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-09-13 21:49:44 +0200

    Secret word hook

--- src/components/SecretWord.js
+++ src/components/SecretWord.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from '../data';
 
 export default function SecretWord() {
+    const [secretWord, setSecretWord] = useState(getRandomWord())
     return (
         <div className="secret-word">The secret word.</div>
     );
+}
+
+function getRandomWord() {
+    return data[Math.floor(Math.random() * data.length)];
 }
--- src/data.js
+++ src/data.js
@@ -1,5 +1,7 @@
-export default data = [
+const data = [
     'apple',
     'mango',
     'strawberry'
 ];
+
+export default data;
