3a1a11bdcc1bcd1d63a793195c9cb594d10162d9
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-09-14 16:18:21 +0200

    Remove function expressions

--- src/components/App.js
+++ src/components/App.js
@@ -16,22 +16,22 @@ export default function App() {
         fetchData();
     }, []);
 
-    const [data, setData] = useState([]);
-    const [secretWord, setSecretWord] = useState();
-    const [letterFields, setLetterFields] = useState(<i>initial value of setLetterFields</i>);
-
     const fetchData = async () => {
         const fetchData = await fetch('https://www.fruitmap.org/api/trees');
         const data = await fetchData.json();
         setData(data);
     }
 
-    const getRandomWord = () => {
+    const [data, setData] = useState([]);
+    const [secretWord, setSecretWord] = useState();
+    const [letterFields, setLetterFields] = useState(<i>initial value of setLetterFields</i>);
+
+    function getRandomWord() {
         // use split so we can work with array of chars
         return data[Math.floor(Math.random() * data.length)].split('');
     }
 
-    const startGame = () => {
+    function startGame() {
         if (typeof data[0] === 'object') {
             setData(data.map(element => {
                 return element.name;
