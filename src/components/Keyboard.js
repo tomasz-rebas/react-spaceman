509a7cc222728d3caeed1dc3ac4cb035f2118ddd
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-09-14 19:41:58 +0200

    Change letter list to array of arrays

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -2,30 +2,61 @@ import React, { useState } from 'react';
 
 export default function Keyboard() {
 
-    const letters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 
-                        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
-                            'z', 'x', 'c', 'v', 'b', 'n', 'm'];
-
-    const [keyboardButtons, setKeyboardButtons] = useState(letters.map(letter => {
-        if (letter === 'p' || letter === 'l') {
-            return (
-                <span key={letter}>
-                    <button>{letter}</button>
-                    <br/>
-                </span>
-            )
-        } else {
-            return (
-                <span key={letter}>
-                    <button>{letter}</button>
-                </span>
-            ) 
-        }
-    }));
+    const [letters, setLetters] = useState([
+        ['q', true],
+        ['w', true],
+        ['e', true],
+        ['r', true],
+        ['t', true],
+        ['y', true],
+        ['u', true],
+        ['i', true],
+        ['o', true],
+        ['p', true],
+        ['a', true],
+        ['s', true],
+        ['d', true],
+        ['f', true],
+        ['g', true],
+        ['h', true],
+        ['j', true],
+        ['k', true],
+        ['l', true],
+        ['z', true],
+        ['x', true],
+        ['c', true],
+        ['v', true],
+        ['b', true],
+        ['n', true],
+        ['m', true]
+    ]);
 
     return (
         <div className="keyboard">
-            {keyboardButtons}
+            {letters.map((letter, index) => {
+                if (letter[0] === 'p' || letter[0] === 'l') {
+                    return (
+                        <span key={letter[0]}>
+                            <button 
+                                disabled={letter[1] ? '' : 'true'}
+                            >
+                                {letter[0]}
+                            </button>
+                            <br/>
+                        </span>
+                    )
+                } else {
+                    return (
+                        <span key={letter[0]}>
+                            <button 
+                                disabled={letter[1] ? '' : 'true'}
+                            >
+                                {letter[0]}
+                            </button>
+                        </span>
+                    )
+                }
+            })}
         </div>
     );
 }
