c9bdc27d32fa8080878f58fd021843a56730bad1
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-09-14 18:27:47 +0200

    Multiplier for picture's dimensions

--- src/components/SpacemanPicture.js
+++ src/components/SpacemanPicture.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 export default function SpacemanPicture() {
+    const sizeMultiplier = 0.6;
     return (
-        <img src="images/spaceman.png" alt="Spaceman"/>
+        <img 
+            src="images/spaceman.png"
+            alt="Spaceman"
+            width={300 * sizeMultiplier}
+            height={400 * sizeMultiplier}
+        />
     );
 }
