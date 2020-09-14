154c7228172cbfa7992dc5410e6cb18340786abb
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2020-09-14 17:55:13 +0200

    Get names in fetchData function

--- src/components/App.js
+++ src/components/App.js
@@ -19,25 +19,17 @@ export default function App() {
     const fetchData = async () => {
         const fetchData = await fetch('https://www.fruitmap.org/api/trees');
         const data = await fetchData.json();
-        setData(data);
+        const dataStripped = data.map(element => {
+            return element.name;
+        });
+        setData(dataStripped);
     }
 
     const [data, setData] = useState([]);
-    const [secretWord, setSecretWord] = useState();
     const [letterFields, setLetterFields] = useState(<i>initial value of setLetterFields</i>);
 
-    function getRandomWord() {
-        // use split so we can work with array of chars
-        return data[Math.floor(Math.random() * data.length)].split('');
-    }
-
     function startGame() {
-        if (typeof data[0] === 'object') {
-            setData(data.map(element => {
-                return element.name;
-            }))
-        }
-        setSecretWord(getRandomWord());
+        const secretWord = data[Math.floor(Math.random() * data.length)].split('');
         setLetterFields(secretWord.map((letter, index) => {
             return <span className="letter-field" key={'letter-field-' + index}>{letter}</span>
         }));
